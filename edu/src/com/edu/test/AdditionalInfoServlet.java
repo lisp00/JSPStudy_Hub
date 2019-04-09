05b1a6b18e24d778909050f5be52511ae90c7a48
A1CK-DHW <A1CK-DHW@DESKTOP-Q36F5OC>
2019-04-09 17:53:33 +0900

    20190409

---
+++ edu/src/com/edu/test/AdditionalInfoServlet.java
@@ -0,0 +1,25 @@
+package com.edu.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/addInfo")
+public class AdditionalInfoServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		res.setContentType("text/html;charset=EUC-KR");
+		PrintWriter out = res.getWriter();
+		out.print("<html>");
+		out.print("<head></head>");
+		out.print("<body>");
+		out.print("<h1>Testing</h1>");
+		out.print("</body>");
+		out.print("</html>");
+	}
+
+}
