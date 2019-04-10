4adad62c4dec84c24ead979da7a415ddb8917bd5
A1CK-DHW <A1CK-DHW@DESKTOP-Q36F5OC>
2019-04-10 18:35:13 +0900

    [Desktop]20190410

--- edu/src/com/edu/test/AdditionalInfoServlet.java
+++ edu/src/com/edu/test/AdditionalInfoServlet.java
@@ -9,20 +9,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/addInfo")
+//@WebServlet("/addInfo")
 public class AdditionalInfoServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		res.setContentType("text/html;charset=EUC-KR");
 		PrintWriter out = res.getWriter();
 		out.print("<html>");
-		out.print("<head></head>");
+		out.print("<head><title>Request 정보 출력</title></head>");
 		out.print("<body>");
-		out.print("<h1>Testing</h1>");
+		out.print("<h3>추가적인 요청 정보</h3>");
+		out.print("Request Method : " + req.getMethod() + "<br/>");
+		out.print("Path Info : " + req.getPathInfo() + "<br/>");
+		out.print("Path Translated : " + req.getPathTranslated() + "<br/>");
+		out.print("Query String : " + req.getQueryString() + "<br/>");
+		out.print("Content Length : " + req.getContentLength() + "<br/>");
+		out.print("Content Type : " + req.getContentType() + "<br/>");
 		out.print("</body>");
 		out.print("</html>");
 		out.close();
-		
-		//This is code for testing
 	}
 
 }
